ddb2ed9afd5da513789c7241675f35f42539ad04
Yanghwa <kellerj87@hotmail.com>
2017-01-18 13:30:49 -0500

    changed let to var, deleted showing computer choice console

--- lab2.js
+++ lab2.js
@@ -1,13 +1,14 @@
-let userChoice = prompt("Do you choose rock, paper or scissors?");
-let computerChoice = Math.random();
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
 if (computerChoice < 0.34) {
 	computerChoice = "rock";
 } else if(computerChoice <= 0.67) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
-let compare = function(choice1, choice2){
+} 
+//console.log("Computer: " + computerChoice);
+var compare = function(choice1, choice2){
     if(choice1===choice2){
         return "The result is a tie!";
     }
